e803c045a4a352a14c06a832bc33f495efcf79a5
Andres Abril <andres.abril@gmail.com>
2017-04-06 02:08:10 -0400

    Added config.json write in frontend folder to get endpoint

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ var logger = require('morgan');
 var bodyParser = require('body-parser');
 var app = express();
 var api = require('./api');
-
+var fs = require('fs');
+var os = require('os');
+var ifaces = os.networkInterfaces();
+var ip = null;
+var json = null;
 //This function allows Angular JS App to consume the service without restriction
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
@@ -27,6 +31,27 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/api', api);
 app.use(express.static(path.join(__dirname, 'frontend')));
 process.env.PORT = 3200;
+
+Object.keys(ifaces).forEach(function (ifname) {
+  var alias = 0;
+
+  ifaces[ifname].forEach(function (iface) {
+    if ('IPv4' !== iface.family || iface.internal !== false) {
+      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+      return;
+    }
+
+    if (alias >= 1) {
+      // this single interface has multiple ipv4 addresses
+    } 
+    else {
+      ip = iface.address
+    }
+    ++alias;
+  });
+});
+json = {"local_endpoint":"http://"+ip+':'+process.env.PORT+'/'};
+fs.writeFile("frontend/config.json", JSON.stringify(json), 'utf8', function(err){}); 
 
 
 // catch 404 and forward to error handler
